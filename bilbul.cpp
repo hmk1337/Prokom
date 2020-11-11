7f9be49bf3eb859f5469f811662e4b024a131908
H Misfal .K <46556282+hmk1337@users.noreply.github.com>
2020-11-11 18:22:25 +0700

    Create bilbul.cpp

---
+++ bilbul.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float bil1,bil2,hasil;
+    cout<<"Masukan Bilangan 1 : ";
+    cin>> bil1;
+    cout<<"Masukan Bilangan 2 : ";
+    cin>> bil2;
+    hasil = bil1 + bil2;
+    cout<<"Hasil :" <<hasil;
+    return 0;
+
+
+}
