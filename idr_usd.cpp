35313b41ab950154f89eff2911189e14b207582a
H Misfal .K <46556282+hmk1337@users.noreply.github.com>
2020-11-11 18:35:12 +0700

    Create idr_usd.cpp

---
+++ idr_usd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float idr,usd,hasil;
+    cout<<"Masukan Jumalah Rupiah anda : ";
+    cin>> idr;
+    usd=9600;
+    hasil = idr/usd;
+    cout<<"hasil jika di konversi ke USD:  $" <<hasil;;
+    return 0;
+
+
+}
