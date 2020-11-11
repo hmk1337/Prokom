689b445cf06151ce06dec039389eeb05ca9c1485
H Misfal .K <46556282+hmk1337@users.noreply.github.com>
2020-11-11 18:25:48 +0700

    Create kecepatan.cpp

---
+++ kecepatan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float v,s,t;
+    cout<<"Masukan Jarak tempuh (Km) : ";
+    cin>> s;
+    cout<<"Masukan Waktu Tempuh (Jam) : ";
+    cin>> t;
+    v = s/t;
+    cout<<"Kecepatan (Km/jam)         :" <<v;
+    return 0;
+
+
+}
