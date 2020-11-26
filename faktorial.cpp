16ccdf037e796c5d61e4fa3906e8d1be14167576
H Misfal .K <46556282+hmk1337@users.noreply.github.com>
2020-11-26 16:05:37 +0700

    Create faktorial.cpp

---
+++ faktorial.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int faktorial(int bil,int hasil){
+    if(bil == 0){
+        cout<<'=';
+        return hasil;
+    }
+    else{
+        hasil*=bil;
+        cout<<bil;
+        if(bil != 1)
+            cout<<'*';
+        faktorial(bil-1,hasil);
+    }
+
+    return 0;
+}
+
+int main(){
+    int bil,hasil=1;
+    cout<<"Masukan bilangan = ";
+    cin>>bil;
+    cout<<"Hitung nilai factorial "<<bil<<" = ";
+    cout<<faktorial(bil,hasil);
+}
